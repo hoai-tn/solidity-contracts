1ae2da910aa095ce1c8c9672ef54e895270d94d3
Hoai Tran <tranngochoai02051999@gmail.com>
2024-04-02 15:35:02 +0700

    Implement ERC721

--- contracts/DOGX.sol
+++ contracts/DOGX.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/extensions/AccessControlEnumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 interface IDogx {
