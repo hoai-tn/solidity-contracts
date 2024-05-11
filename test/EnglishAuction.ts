b049eb51a4bd07aba811b7e240db814f3dfa27c6
Hoai Tran <tranngochoai02051999@gmail.com>
2024-05-11 09:36:01 +0700

    [TASK] Finished EnglishAuction testing

--- test/EnglishAuction.ts
+++ test/EnglishAuction.ts
@@ -2,9 +2,6 @@ import { DOGX, EnglishAuction } from "../typechain-types";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
-const {
-  loadFixture,
-} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 
 function parseEther(amount: Number) {
   return ethers.parseEther(amount.toString());
@@ -101,9 +98,36 @@ describe("English Auction Contract", function () {
       parseInt(ethers.formatEther(balanceOfBobAfterWithdraw - parseEther(3)))
     );
   });
-});
 
-it("Should end auction", async () => {});
+  it("Should end auction", async () => {
+    const { nft, auction, owner, seller, bob, carol } =
+      await deployTokenFixture();
+
+    await startAuction({ nft, auction, owner, seller });
+
+    await auction.connect(bob).bid({ value: parseEther(3) });
+    await auction.connect(carol).bid({ value: parseEther(4) });
+    await auction.connect(bob).bid({ value: parseEther(5) });
+
+    const balanceOfSellerBeforeEnd = await ethers.provider.getBalance(
+      seller.address
+    );
+    // increase timestamp to end auction
+    await ethers.provider.send("evm_increaseTime", [86400 * 7]); // Set the block time to the specified timestamp
+    await ethers.provider.send("evm_mine");
+
+    await auction.end();
+    const balanceOfSellerAfterEnd = await ethers.provider.getBalance(
+      seller.address
+    );
+    const ownerOfNft = await nft.ownerOf(1);
+
+    expect(ownerOfNft).eq(bob.address);
+    expect(parseInt(ethers.formatEther(balanceOfSellerAfterEnd))).to.equal(
+      parseInt(ethers.formatEther(balanceOfSellerBeforeEnd + parseEther(5)))
+    );
+  });
+});
 
 const startAuction = async ({
   nft,
@@ -121,4 +145,7 @@ const startAuction = async ({
   await nft.connect(seller).approve(await auction.getAddress(), 1);
 
   await auction.connect(seller).start();
+  const ownerOfNft = await nft.ownerOf(1);
+
+  expect(await auction.getAddress()).eq(ownerOfNft);
 };
