43677a21bad52e76358d8b477a9fc175e77334f8
Hoai Tran <tranngochoai02051999@gmail.com>
2024-07-11 09:24:59 +0700

    [DEPLOY] Deploy CTC CrowdFunding

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,4 @@
-import { ethers, hardhatArguments } from "hardhat";
+import { config, ethers, hardhatArguments } from "hardhat";
 import * as Config from "./config";
 
 async function main() {
@@ -31,18 +31,18 @@ async function main() {
   // console.log("USDT address: ", usdtAddress);
   // Config.setConfig(network + ".USDT", usdtAddress);
 
-  const HDZCrowdsale = await ethers.getContractFactory("CTCPresale");
-  const hdzCrowdsale = await HDZCrowdsale.deploy(
-    deployer.address,
-    3000,
-    1,
-    "0xD7b410228a27Cb885f2da9971c2c875B0757a905",
-    "0x3f18BC29773bb0E5129a7FBF1Bb67FCE1ac6B930"
-  );
+  // const HDZCrowdsale = await ethers.getContractFactory("CTCPresale");
+  // const hdzCrowdsale = await HDZCrowdsale.deploy(
+  //   deployer.address,
+  //   3000,
+  //   1,
+  //   "0xD7b410228a27Cb885f2da9971c2c875B0757a905",
+  //   "0x3f18BC29773bb0E5129a7FBF1Bb67FCE1ac6B930"
+  // );
 
-  const hdzCrowdsaleAddress = await hdzCrowdsale.getAddress();
-  console.log("HDZCrowdsale address: ", hdzCrowdsaleAddress);
-  Config.setConfig(network + ".CTCPresale", hdzCrowdsaleAddress);
+  // const hdzCrowdsaleAddress = await hdzCrowdsale.getAddress();
+  // console.log("HDZCrowdsale address: ", hdzCrowdsaleAddress);
+  // Config.setConfig(network + ".CTCPresale", hdzCrowdsaleAddress);
 
   // const DOGX = await ethers.getContractFactory("DOGX");
   // const dogX = await DOGX.deploy("0x6FeefDD0445Ded0f111BE915f775c8D41C361611");
@@ -50,12 +50,11 @@ async function main() {
   // console.log("DOGX address: ", dogXAddress);
   // Config.setConfig(network + ".DOGX", dogXAddress);
 
-  // const CrowdFund = await ethers.getContractFactory("crowdFund");
-  // const crowdFund = await CrowdFund.deploy(
-  // );
-  // const crowdFundAddress = await crowdFund.getAddress();
-  // console.log("crowdFund address: ", crowdFundAddress);
-  // Config.setConfig(network + ".crowdFund", crowdFundAddress);
+  const CrowdFund = await ethers.getContractFactory("CrowdFunding");
+  const crowdFund = await CrowdFund.deploy("0x3f18bc29773bb0e5129a7fbf1bb67fce1ac6b930");
+  const crowdFundAddress = await crowdFund.getAddress();
+  console.log("crowdFund address: ", crowdFundAddress);
+  Config.setConfig(network + ".CtcCrowdFunding", crowdFundAddress);
 
   await Config.updateConfig();
 }
